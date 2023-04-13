e466920edc975feb1ffeb704a54c565f61a38d2e
wilson-gsc <wilsongayo@golden-success.com>
2023-04-13 15:34:09 +0800

    updated sequelize

--- _helpers/db.js
+++ _helpers/db.js
@@ -13,7 +13,10 @@ async function initialize() {
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, {
+        host,
+        dialect: 'mysql'
+      });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
