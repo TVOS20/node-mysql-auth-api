ecc8a8b484f4dacbba259922a8354b1324c87e71
unknown <YY128@ccs.local>
2023-04-25 20:34:27 +0800

    update1

--- _middleware/error-handler.js
+++ _middleware/error-handler.js
@@ -7,6 +7,8 @@ function errorHandler(err, req, res, next) {
             const is404 = err.toLowerCase().endsWith('not found');
             const statusCode = is404 ? 404 : 400;
             return res.status(statusCode).json({ message: err });
+        case err.name === 'UnauthorizedError':
+            return res.status(401).json({ message: 'Unauthorized' });
         default:
             return res.status(500).json({ message: err.message });
     }
--- users/user.service.js
+++ users/user.service.js
@@ -1,13 +1,27 @@
 ﻿const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
+    authenticate,
     getAll,
     getById,
     create,
     update,
     delete: _delete
 };
+
+async function authenticate({ username, password }) {
+    const user = await db.User.scope('withHash').findOne({ where: { username } });
+
+    if (!user || !(await bcrypt.compare(password, user.hash)))
+        throw 'Username or password is incorrect';
+
+    // authentication successful
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+    return { ...omitHash(user.get()), token };
+}
 
 async function getAll() {
     return await db.User.findAll();
